faf1a18ed1d1ad50c90729489770b33e2a26bdb1
Veronica <veronica.celemin@gmail.com>
2022-12-20 15:17:06 +0100

    feat: show favorites in Favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,22 @@
+import { useSelector } from "react-redux";
+import MeetupItem from "../components/meetups/MeetupItem";
+import classes from "../components/meetups/MeetupList.module.css";
+
 export default function FavoritesPage() {
+  const { meetupList, favoriteList } = useSelector((state) => state);
+
   return (
     <section>
       <h1>Favorites Page</h1>
+      {favoriteList && !!favoriteList.length && (
+        <ul className={classes.list}>
+          {meetupList
+            .filter((currentMeetup) => favoriteList.includes(currentMeetup.id))
+            .map((currentItem) => (
+              <MeetupItem key={currentItem.id} item={currentItem} />
+            ))}
+        </ul>
+      )}
     </section>
   );
 }
