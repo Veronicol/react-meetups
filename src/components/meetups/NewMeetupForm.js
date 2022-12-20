ac6acde89bac9bd07d092a5c7ca0137bde3f9918
Veronica <veronica.celemin@gmail.com>
2022-12-20 09:35:04 +0100

    feat: control NewMeetupForm form fields

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,32 +1,83 @@
+import { useState } from "react";
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 export default function NewMeetupForm() {
+  const fieldsMap = {
+    TITLE: "title",
+    IMAGEURL: "image",
+    ADDRESS: "address",
+    DESCRIPTION: "description",
+  };
+
+  const { TITLE, IMAGEURL, ADDRESS, DESCRIPTION } = fieldsMap;
+
+  const [formData, setFormData] = useState({
+    title: "",
+    image: "",
+    address: "",
+    description: "",
+  });
+
   function submitHandler(event) {
     event.preventDefault();
+    if (formData.title && formData.image && formData.address && formData.description) {
+      // TODO: ADD TO GLOBAL STATE OF APP
+    }
   }
+
+  const handleInputChange = (field, event) => {
+    setFormData({
+      ...formData,
+      [field]: event.target.value,
+    });
+  };
 
   return (
     <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <form className={classes.form} onSubmit={(event) => submitHandler(event)}>
         <div className={classes.control}>
-          <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <label htmlFor={TITLE}>Meetup Title</label>
+          <input
+            type="text"
+            required
+            id={TITLE}
+            value={formData.title}
+            onChange={(event) => handleInputChange(TITLE, event)}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <label htmlFor={IMAGEURL}>Meetup Image</label>
+          <input
+            type="url"
+            required
+            id={IMAGEURL}
+            value={formData.image}
+            onChange={(event) => handleInputChange(IMAGEURL, event)}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <label htmlFor={ADDRESS}>Address</label>
+          <input
+            type="text"
+            required
+            id={ADDRESS}
+            value={formData.address}
+            onChange={(event) => handleInputChange(ADDRESS, event)}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor="description">Description</label>
-          <textarea id="description" required rows="5"></textarea>
+          <label htmlFor={DESCRIPTION}>Description</label>
+          <textarea
+            id={DESCRIPTION}
+            required
+            rows="5"
+            value={formData.description}
+            onChange={(event) => handleInputChange(DESCRIPTION, event)}
+          />
         </div>
         <div className={classes.actions}>
-          <button>Add Meetup</button>
+          <button type="submit">Add Meetup</button>
         </div>
       </form>
     </Card>
