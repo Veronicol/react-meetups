a07b5a422e61155b44a6a357bc9b68c7d2aadd3d
Veronica <veronica.celemin@gmail.com>
2022-12-20 12:42:33 +0100

    feat: dispatch initial meetup list from AllMeetupsPage

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,20 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import MeetupItem from "../components/meetups/MeetupItem";
 import classes from "../components/meetups/MeetupList.module.css";
-import { useFetch } from "../util-hooks/useFetch";
+import { fetchInitialMeetupList } from "../redux/actions/meetup_list";
 
 export default function AllMeetupsPage() {
-  const { data } = useFetch({
-    url: "/data.json",
-  });
+  const { meetupList } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  if (!data) return <p>Loading...</p>;
+  useEffect(() => {
+    if (!meetupList.length) {
+      dispatch(fetchInitialMeetupList());
+    }
+  }, []);
 
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul className={classes.list}>
-        {data && data.map((currentItem) => <MeetupItem key={currentItem.id} item={currentItem} />)}
-      </ul>
+      {meetupList && !!meetupList.length && (
+        <ul className={classes.list}>
+          {meetupList.map((currentItem) => (
+            <MeetupItem key={currentItem.id} item={currentItem} />
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
